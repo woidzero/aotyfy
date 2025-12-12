234e81b6981724bfa0f0bd15b17e7386a365e8dd
woidzero <71274141+woidzero@users.noreply.github.com>
2025-12-12 19:50:42 +0100

    fix

--- install.bat
+++ install.bat
@@ -1,15 +1,15 @@
 @echo off
 setlocal
 
-set "URL=https://github.com/woidzero/aotyfy/raw/main/aotyfy.js"
-
+set "URL=https://raw.githubusercontent.com/woidzero/aotyfy/refs/heads/master/dist/aotyfy.js"
 set "TARGET=%APPDATA%\spicetify\Extensions"
 
 if not exist "%TARGET%" (
     mkdir "%TARGET%"
 )
 
-powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%TARGET%\aotyfy.js'"
+powershell -Command "Invoke-WebRequest -Uri \"%URL%\" -OutFile \"%TARGET%\aotyfy.js\""
 
 echo AOTYFY installed.
+
 pause
